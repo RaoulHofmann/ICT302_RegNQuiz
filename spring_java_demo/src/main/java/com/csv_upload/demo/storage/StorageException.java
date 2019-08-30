39b8219e8d40db0bf66d754e6b65f9872e520f8b
Raoul <raouldhofmann@gmail.com>
2019-08-30 17:07:22 +0800

    Spring file upload testing
    Application not working yet problem with StorageService bean

---
+++ spring_java_demo/src/main/java/com/csv_upload/demo/storage/StorageException.java
@@ -0,0 +1,12 @@
+package com.csv_upload.demo.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
