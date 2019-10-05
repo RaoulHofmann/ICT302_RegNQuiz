315902d3d0f870182532b14ec697ddf056fa7d47
mattmac12 <m.maclennan@hotmail.com>
2019-10-05 19:48:59 +0800

    missed imports

--- src/main/java/com/regnquiz/model/StaffImport.java
+++ src/main/java/com/regnquiz/model/StaffImport.java
@@ -5,10 +5,58 @@
  */
 package com.regnquiz.model;
 
+import com.regnquiz.model.repositories.UserRepository;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author Matthew MacLennan
  */
-public class StaffImport {
+@Service
+public class StaffImport 
+{
+    @Autowired
+    private UserRepository userRepo;
     
+    @Transactional
+    public void ImportStaff(String filename)
+    {
+        Path myPath = Paths.get(filename);    
+        try
+        {
+            List<String> lines = Files.readAllLines(myPath);
+            
+            lines.remove(0);
+            
+            for(String line : lines)
+            {
+                String[] lineSplit = line.split(",");
+                
+                // Staff Number, Given Names, Last Name
+                User staff = new User(Integer.parseInt(lineSplit[0]), lineSplit[1], lineSplit[2]);
+                
+                try 
+                {
+                    staff = userRepo.save(staff);
+                }
+                catch (DataIntegrityViolationException ex)
+                {
+                    System.out.println("CSV Error: " + ex);
+                }
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("ERROR IOException: " + e);
+        }
+    }
 }
+ 
